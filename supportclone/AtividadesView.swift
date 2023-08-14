7ddb4f122f20b5a7d0897827d1ba08f504775ee6
Dedeti <andrenes2905@gmail.com>
2023-08-14 15:25:28 -0300

    Adicionar a tela de Atividades Recentes

---
+++ supportclone/AtividadesView.swift
@@ -0,0 +1,35 @@
+//
+//  AtividadesView.swift
+//  supportclone
+//
+//  Created by André Enes Pecci on 14/08/23.
+//
+
+import SwiftUI
+
+struct AtividadesView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image(systemName: "clock.fill")
+                    .font(.system(size: 50))
+                    .padding()
+                    .foregroundColor(.gray)
+                Text("Nenhuma atividade de suporte nos últimos 90 dias")
+                    .bold()
+                    .font(.system(size: 25))
+                    .multilineTextAlignment(.center)
+                Text("Reservas, chamadas e bate-papos passados e futuros com o Suporte da Apple serão exibidos aqui.")
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.gray)
+            }
+            .navigationTitle("Atividades Recentes")
+        }
+    }
+    
+    struct AtividadesView_Previews: PreviewProvider {
+        static var previews: some View {
+            AtividadesView()
+        }
+    }
+}
